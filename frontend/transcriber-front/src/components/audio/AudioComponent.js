22639dac1b9b4b7e4b00e23dbf3ab288074ac8d3
Kamil Faber <faber.kamil@gmail.com>
2020-06-05 21:12:22 +0200

    Sending audio moved to webRTC protocol - it still needs to be finished when we have server

--- frontend/transcriber-front/src/components/audio/AudioComponent.js
+++ frontend/transcriber-front/src/components/audio/AudioComponent.js
@@ -5,46 +5,26 @@ export class AudioComponent extends React.Component {
         super(props);
         this.state = {
             audio: null,
-            recorder: null,
-            recordedData: null,
-            recordingSrc: null,
         };
 
-        this.websocket = new WebSocket("ws://192.168.0.1");
+        this.rtcConnection = new RTCPeerConnection();
         this.toggleMicrophone = this.toggleMicrophone.bind(this);
-        this.handleBlob = this.handleBlob.bind(this);
-        this.getMicrophone = this.getMicrophone.bind(this);
-        this.stopMicrophone = this.stopMicrophone.bind(this);
-        this.toggleMicrophone = this.toggleMicrophone.bind(this);
-        this.intervalCall = this.intervalCall.bind(this);
-        setInterval(this.intervalCall, 1000)
+        this.turnOnMicrophone = this.turnOnMicrophone.bind(this);
+        this.turnOffMicrophone = this.turnOffMicrophone.bind(this);
     }
 
-    handleBlob = (data) => {
-        this.setState({recordedData: data});
-        this.websocket.send(data);
-    };
-
-    intervalCall = () => {
-        if(this.state.audio) {
-            this.state.recorder.stop();
-            this.state.recorder.start()
-        }
-    };
-
-    async getMicrophone() {
+    async turnOnMicrophone() {
         const audio = await navigator.mediaDevices.getUserMedia({
             audio: true,
             video: false
         });
+        this.rtcConnection.addStream(audio)
+        audio.getTracks().forEach(track => this.rtcConnection.addTrack(track, audio));
 
-        let mediaRecorder = new MediaRecorder(audio, {mimeType: 'audio/ogg'});
-        mediaRecorder.ondataavailable = (e) => this.handleBlob(e.data);
-        mediaRecorder.start();
-        this.setState({audio, recorder: mediaRecorder});
+        this.setState({audio});
     }
 
-    stopMicrophone() {
+    turnOffMicrophone() {
         this.state.audio.getTracks().forEach(track => track.stop());
         this.state.recorder.stop();
         this.setState({audio: null, recorder: null, recordedData: null});
@@ -52,9 +32,9 @@ export class AudioComponent extends React.Component {
 
     toggleMicrophone() {
         if (this.state.audio) {
-            this.stopMicrophone();
+            this.turnOffMicrophone();
         } else {
-            this.getMicrophone();
+            this.turnOnMicrophone();
         }
     }
 
@@ -62,13 +42,8 @@ export class AudioComponent extends React.Component {
         return (
             <div className="controls">
                 <button onClick={this.toggleMicrophone}>
-                    {this.state.audio ? 'Stop microphone' : 'Turn on microphone'}
+                    {this.state.audio ? 'Turn off microphone' : 'Turn on microphone'}
                 </button>
-
-                {this.state.recordedData && <audio id={"replay"} controls>
-                    <source src={URL.createObjectURL(this.state.recordedData)} type={"audio/ogg"} />
-                    Your browser does not support the audio element.
-                </audio>}
             </div>
         );
     }
