c2b482af631c9f26832dcd1a266d400ddd44b73e
Cristian Schuszter <cristian.schuszter@cern.ch>
2020-06-06 02:17:23 +0200

    Restart stale conns

--- backend/messaging/rmq.py
+++ backend/messaging/rmq.py
@@ -10,30 +10,36 @@ class Connecter:
     def __init__(self):
         self.connection = None
 
+    def _connect(self):
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(
+            self.host, virtual_host=self.vhost, credentials=self.creds)
+        )
+
     def init_app(self, app):
         creds = pika.PlainCredentials(
             app.config.get('RMQ_USER'), app.config.get('RMQ_PASS')
         )
+        self.creds = creds
+        self.host = app.config.get('RMQ_HOST')
+        self.vhost = app.config.get("RMQ_VHOST")
 
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters(
-            app.config.get('RMQ_HOST'), virtual_host=app.config.get("RMQ_VHOST"), credentials=creds)
-        )
-
-        self.channel = self.connection.channel()
+        self._connect()
 
     def define_queue(self, queue_name):
         channel = self.connection.channel()
         channel.queue_declare(queue=queue_name)
 
     def listen(self, queue, callback):
-
-        self.channel.basic_consume(queue=queue,
-                                   auto_ack=True,
-                                   on_message_callback=callback)
+        self._connect()
+        channel = self.connection.channel()
+        channel.basic_consume(queue=queue,
+                              auto_ack=True,
+                              on_message_callback=callback)
         logging.info(
             f' [*] Waiting for messages on {queue}. To exit press CTRL+C')
-        self.channel.start_consuming()
+        channel.start_consuming()
 
     def produce(self, queue, message):
+        self._connect()
         channel = self.connection.channel()
         channel.basic_publish('', routing_key=queue, body=message)
