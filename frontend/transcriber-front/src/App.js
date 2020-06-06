51a558d016ad0b878ad22c1a6dbffba8b7dd98d6
Cristian Schuszter <cristian.schuszter@cern.ch>
2020-06-06 14:08:17 +0200

    Rebase

--- frontend/transcriber-front/src/App.js
+++ frontend/transcriber-front/src/App.js
@@ -1,43 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import './components/audio/AudioComponent'
-import {rtcConnectionService} from "./services/RTCConnectionService";
-import {LoginComponent} from "./components/login/LoginComponent";
-import 'antd/dist/antd.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./components/audio/AudioComponent";
+import { rtcConnectionService } from "./services/RTCConnectionService";
+import { LoginComponent } from "./components/login/LoginComponent";
+import "antd/dist/antd.css";
 
-import {MeetingComponent} from "./components/meetings/MeetingComponent";
-import {Button} from "antd";
+import { MeetingComponent } from "./components/meetings/MeetingComponent";
+import { Button } from "antd";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: undefined
-        }
-    }
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <p>
-                        Hello {this.state.username}!
-                    </p>
-                    {!this.state.username && <LoginComponent onFinish={value => this.setState({username: value.username})}/>}
-                </header>
-                <div className="main-container">
-                    <MeetingComponent />
-                </div>
-                <div>
-                    <Button type="primary" onClick={rtcConnectionService.initConnection}>
-                        Init connection
-                    </Button>
-
-                </div>
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: undefined,
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Hello {this.state.username}!</p>
+          {!this.state.username && (
+            <LoginComponent
+              onFinish={(value) => this.setState({ username: value.username })}
+            />
+          )}
+        </header>
+        <div className="main-container">
+          <MeetingComponent />
+        </div>
+        <div>
+          <Button type="primary" onClick={rtcConnectionService.initConnection}>
+            Init connection
+          </Button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
